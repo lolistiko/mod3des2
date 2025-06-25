e1d3bc2649a3b5326369b4e517c3ba34c1792754
Rodrigo Irarrazabal <129896504+lolistiko@users.noreply.github.com>
2025-06-25 00:07:26 -0400

    Update imagen_borde.js
    actualizacion px

--- imagen_borde.js
+++ imagen_borde.js
@@ -2,11 +2,11 @@ const imgChangeStyle = document.querySelector("#imgChangeStyle");
 const btnChangeStyle = document.querySelector("#btnChangeStyle");
 
 btnChangeStyle.addEventListener("click", function (event) {
-  if (imgChangeStyle.style.border == "30px solid yellow") {
+  if (imgChangeStyle.style.border == "2px solid yellow") {
     imgChangeStyle.style.border = "none";
     btnChangeStyle.innerHTML = "Explota su KI";
   } else {
-    imgChangeStyle.style.border = "30px solid yellow";
+    imgChangeStyle.style.border = "2px solid yellow";
     btnChangeStyle.innerHTML = "vuelve a su estado";
   }
 });
